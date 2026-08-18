c71f2d0060be1108754ff6c5f30051712bd593ff
uaauuayatat-ops <uaauuayatat@gmail.com>
2026-08-18 12:46:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,16 +4,22 @@ const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./css/style.css",
+
+  "./CSS/style.css",
+
   "./js/app.js",
   "./js/data.js",
-  "./js/firebase-config.js"
+  "./js/firebase-config.js",
+
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 
   self.skipWaiting();
@@ -35,7 +41,8 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
