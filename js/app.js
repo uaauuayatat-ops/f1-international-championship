b896f1397a62c3aaf3de6db2652235839950b2a4
uaauuayatat-ops <uaauuayatat@gmail.com>
2026-08-26 19:26:42 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -183,7 +183,9 @@ function recalcOdds() {
     acosta: 8.50, mauricio: 12.00, vidal: 19.00, pichardo: 23.00,
     osorio: 26.00, flores: 29.00, maximo: 31.00, caceres: 34.00,
     jeanfranco: 41.00, raneri: 51.00, camilo: 67.00, dave: 81.00,
-    chichar: 101.00, tapara: 105.00, luca: 107.00, hernandez: 110.00
+    chichar: 101.00, tapara: 105.00, luca: 107.00,     hernandez: 55.00, catnap: 102.00,
+    yzaac: 110.00, ianfalla: 63.00, ventura: 102.00,
+    santiago: 102.00, hitan: 102.00, jose: 102.00, agustin: 102.00
   };
 
   function getDriverId(d) {
